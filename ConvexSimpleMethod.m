ac43e71140156c3321a505b3244cb72dab94b2d8
RennieMaster2000 <menkveldrenee@gmail.com>
2026-01-08 21:57:21 +0100

    initial simple method

---
+++ ConvexSimpleMethod.m
@@ -0,0 +1,4 @@
+L=1/2;
+b = [0.1, 0.5,-1,0];
+g = gfunc(b)
+alpha = alphafunc(0,L)
---
+++ alphafunc.m
@@ -0,0 +1,5 @@
+function [alpha] = alphafunc(k,L)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+alpha=L/(2^(k+1));
+end
