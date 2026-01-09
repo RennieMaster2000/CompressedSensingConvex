44b25822aed6080c963a3052c8c73ffda0fa1c83
RennieMaster2000 <menkveldrenee@gmail.com>
2026-01-09 19:31:26 +0100

    most recent function

--- projAx_b.m
+++ projAx_b.m
@@ -1,6 +1,6 @@
 function [xproj] = projAx_b(x,A,b)
 %PROJAX_B Summary of this function goes here
 %   Detailed explanation goes here
-xproj=x-A'*((A*A')\(A*x-b));
+xproj=x-A'*pinv(A*A')*(A*x-b);
 end
 
