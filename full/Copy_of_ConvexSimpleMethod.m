ff007c2d363fd69aaf743a7f1082fa0eb580381b
RennieMaster2000 <menkveldrenee@gmail.com>
2026-01-11 11:36:55 +0100

    1st unsuccessful attempt

--- full/Copy_of_ConvexSimpleMethod.m
+++ full/Copy_of_ConvexSimpleMethod.m
@@ -2,7 +2,7 @@ clear; clc;
 load('cs.mat');
 N = n;
 
-max_iter = 1000;
+max_iter = 100;
 L=0.5;
 tol = 1e-6;
 
@@ -13,15 +13,21 @@ b = X_us;
 xold = ones(N,1)/2; %initial
 fbest = n;%ffunc(xold);
 xbest = xold;
+kbest = 0;
 
 iter = 0;
 for k = 1:max_iter
 
     %Polyak step
     gamma = 1/k;
-    alpha = ffunc(xold)-fbest+gamma; % does not work due to f_best being outside constraints
-
-    alpha = alphafunc(k,L);
+    if k <= 11
+        alpha = gamma;
+    else
+        alpha = ffunc(xold)-fbest+gamma; % does not work due to f_best being outside constraints
+        %alpha
+    end
+    alpha
+    %alpha = alphafunc(k,L);
     xnew = xold - alpha*gfunc(xold);% Subgradient step 
     xnew = projAx_b(xnew,A,b);  % Projection onto equality constraint
     xnew = max(real(xnew), 0);  % Projection onto nonnegativity
@@ -33,6 +39,7 @@ for k = 1:max_iter
     if fnew < fbest && k>10
         fbest = fnew;
         xbest = xnew;
+        kbest = k;
     end
     if norm(xnew - xold) < tol && k>10% Stop when within tolerance
         break 
@@ -42,6 +49,7 @@ for k = 1:max_iter
 end
 %fbest = fnew;
 fbest
+kbest
 %xbest = xnew;
 fprintf('Algorithm stopped after %d iterations\n', iter);
 
